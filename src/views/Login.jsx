62a50d4782f82e414f3007b95337a55a31f12294
brayan-92 <114324477+brayan-92@users.noreply.github.com>
2024-11-07 17:00:37 -0500

    Update Login.jsx

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('https://proyecto-code-prom-front.vercel.app/api/login', {
+      const response = await fetch('https://proyecto-code-prom-back.vercel.app/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
